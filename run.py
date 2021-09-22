cddf6ca2979235851b59b6c81508e01314c0c55f
Arek Nowakowski <nowakowski.arkadiusz@gmail.com>
2021-09-22 20:49:11 +0200

    Add results as table

--- run.py
+++ run.py
@@ -21,13 +21,20 @@ experiments = [
 
 algorithms = ["ID3", "C4.5", "CART", "CHAID"]
 
+data = {}
+
 for experiment in experiments:
+
+    data[experiment["id"]] = {}
+
     names = list(map(lambda x: str(x), range(experiment["nofColumns"] - 1))) + ["Decision"]
 
     print("-------------------------")
     print("Experiment ID:", experiment['id'])
 
     for algorithm in algorithms:
+
+        data[experiment["id"]][algorithm] = []
 
         print("-------------------------")
         print("Algorithm:", algorithm)
@@ -77,7 +84,67 @@ for experiment in experiments:
             predicted = chef.predict(model, instance)
             actual = instance['Decision']
             results[(str(actual), str(predicted))] = results[(str(actual), str(predicted))] + 1
+        
         for i, j in enumerate(sorted(results.items(), key=lambda item: item[1], reverse=True)):
-            print("{\"" + j[0][0] + "\", \"" + j[0][1] + "\"} => " + str(j[1]) + ("," if i < len(results) - 1 else ""))
+            #print("{\"" + j[0][0] + "\", \"" + j[0][1] + "\"} => " + str(j[1]) + ("," if i < len(results) - 1 else ""))
+            data[experiment["id"]][algorithm].append((j[0], j[1]))
 
-    print("\n") # make some space for the next experiment
+# Database balance-scale:
+data["balance-scale"]["mts"] = [(("R", "R"), 70), (("L", "L"), 77), (("B", "R"), 10), (("R", "L"), 6), (("L", "R"), 6), (("R", "B"), 9), (("L", "B"), 4), (("B", "L"), 6)]
+# Database breast-cancer:
+data["breast-cancer"]["mts"] = [(("no", "yes"), 4), (("no", "no"), 61), (("yes", "yes"), 4), (("yes", "no"), 15)]
+# Database car:
+data["car"]["mts"] = [(("acc", "acc"), 112), (("unacc", "unacc"), 364), (("vgood", "vgood"), 13), (("good", "good"), 14), (("unacc", "good"), 1), (("unacc", "acc"), 4), (("acc", "good"), 1), (("vgood", "good"), 3), (("good", "vgood"), 2), (("acc", "unacc"), 4), (("good", "acc"), 1)]
+# Database lymphography:
+data["lymphography"]["mts"] = [(("2", "2"), 21), (("3", "2"), 7), (("3", "3"), 10), (("2", "3"), 4), (("2", "1"), 1), (("2", "4"), 1), (("4", "2"), 1)]
+# Database tic-tac-toe:
+data["tic-tac-toe"]["mts"] = [(("positive", "positive"), 192), (("negative", "negative"), 83), (("negative", "positive"), 11), (("positive", "negative"), 2)]
+
+for experiment in experiments:
+    print("## Dataset:", experiment["id"])
+    names = list(map(lambda x: str(x), range(experiment["nofColumns"] - 1))) + ["Decision"]
+    trainingDF = pd.read_csv("datasets/" + experiment["training"], header=None, names=names)
+    validationDF = pd.read_csv("datasets/" + experiment["validation"], header=None, names=names)
+    testDF = pd.read_csv("datasets/" + experiment["test"], header=None, names=names)
+    trainingDF['Decision'] = trainingDF['Decision'].astype(object)
+    validationDF['Decision'] = validationDF['Decision'].astype(object)
+    testDF['Decision'] = testDF['Decision'].astype(object)
+    labels = list(set(trainingDF['Decision'].tolist() + testDF['Decision'].tolist()))
+    algorithms = data[experiment["id"]]
+    print("| Algorithm | Decision class | Accuracy | Precision | Recall | F1 |")
+    print("| --- | --- | --- | --- | --- | --- |")
+    for algorithm in algorithms:
+        for decision_class in labels:
+            decision_class = str(decision_class)
+            fp = 0; fn = 0; tp = 0; tn = 0
+            for (actual, prediction), count  in data[experiment["id"]][algorithm]:
+                if actual == decision_class and prediction == decision_class:
+                    tp = tp + count
+                elif actual != decision_class and prediction != decision_class:
+                    tn = tn + count
+                elif actual != decision_class and prediction == decision_class:
+                    fp = fp + count
+                elif actual == decision_class and prediction != decision_class:
+                    fn = fn + count
+            epsilon = 0.0000001 #to avoid divison by zero exception
+            precision = round(100*tp / (tp + fp + epsilon), 4)
+            recall = round(100*tp / (tp + fn + epsilon), 4) #tpr
+            f1_score = round((2 * precision * recall) / (precision + recall + epsilon), 4)
+            accuracy = round(100 * (tp + tn) / (tp + tn + fp + fn + epsilon), 4)
+            print("| " + algorithm, end="")
+            print(" | " + decision_class + " | ", end = '')
+            print(accuracy,"% | ", end='')
+            print(precision,"% | ", end='')
+            print(recall,"% | ", end='')
+            print(f1_score,"% |")
+        
+
+'''
+
+    
+    
+    for decision_class in labels:
+        
+        #if len(labels) < 3:
+        #    break
+'''
